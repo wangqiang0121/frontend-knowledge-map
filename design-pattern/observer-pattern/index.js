60d081d828cf20aac1bb068ca2132e0163ba6ebc
Wang Qiang <wangqiang0121@qq.com>
2019-01-31 11:44:26 +0800

    添加观察者模式

---
+++ design-pattern/observer-pattern/index.js
@@ -0,0 +1,49 @@
+/**
+ * 观察者模式
+ * Subject <--> Observer
+ */
+class Subject {
+  constructor() {
+    this.observers = [];
+  }
+
+  addObserver(f) {
+    this.observers.push(f)
+    console.log(`${f.name} 进入队列`)
+  }
+
+  removeObserver(f) {
+    this.observers = this.observers.filter(subscribe => subscribe !== f)
+    console.log(`${f.name} 离开队列`)
+  }
+
+  notify(data) {
+    this.observers.forEach(observer => observer.run(data));
+  }
+}
+
+class Observer {
+  constructor (name) {
+    this.name = name
+  }
+
+  run (data) {
+    console.log(`${this.name} 响应命令 ${data}`)
+  }
+}
+
+const subject = new Subject()
+const person1 = new Observer('小明')
+const person2 = new Observer('小红')
+const person3 = new Observer('小张')
+
+subject.addObserver(person1)
+subject.addObserver(person2)
+subject.addObserver(person3)
+
+subject.notify('向左')
+
+subject.removeObserver(person2)
+
+subject.notify('向右')
+
