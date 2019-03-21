82fc55a88c2f2bc25eebf5e15e005d38521c5e99
Wang Qiang <wangqiang0121@qq.com>
2019-03-21 11:21:42 +0800

    feat: 学习JavaScript运行机制

---
+++ execution/EventLoop.js
@@ -0,0 +1,254 @@
+/*
+  [前端基础进阶（十二）：深入核心，详解事件循环机制](https://www.jianshu.com/p/12b9f73c5a4f)
+  这篇文章里面提到的分析方法非常有用。
+*/
+
+/**
+ * Example 1
+ * result: 输出顺序a,b,c
+ * why: Promise的resolve始终都是异步操作
+ */
+var r = new Promise(function (reslove, reject) {
+  console.log('a') // 构造函数直接执行
+  reslove() // 进入微任务队列
+});
+
+r.then(() => console.log('c'))
+
+console.log('b') // 宏任务
+
+/**
+ * Example 2
+ * result: 输出顺序a,b,c,d
+ * why: Promise产生的是JavaScript引擎内部的微任务，而setTimeout是浏览器API，它产生宏任务
+ */
+var r = new Promise(function (reslove, reject) {
+  console.log('a')
+  reslove()
+});
+// 宏任务
+setTimeout(() => {
+  console.log('d')
+}, 0);
+// 微任务
+r.then(() => console.log('c'))
+console.log('b')
+
+
+/**
+ * Example 3
+ * result: 输出顺序c1,c2,d
+ * why: 微任务优先
+ */
+// 宏任务
+setTimeout(() => console.log("d"), 0)
+var r1 = new Promise(function (resolve, reject) {
+  resolve()
+});
+r1.then(() => {
+  // 微任务
+  var begin = Date.now();
+  while (Date.now() - begin < 1000);
+  console.log("c1")
+  new Promise(function (resolve, reject) {
+    // 微任务
+    resolve()
+  }).then(() => console.log("c2"))
+});
+
+/**
+ * Example 4
+ * result: a,b,c
+ * why: 微任务优先
+ */
+function sleep(duration) {
+  return new Promise(function (resolve, reject) {
+    // 宏任务
+    console.log("b");
+    // 宏任务
+    setTimeout(resolve, duration);
+  })
+}
+// 宏任务
+console.log("a");
+// 微任务
+sleep(5000).then(() => console.log("c"));
+
+
+/**
+ * Example 5
+ * result: 2，3，5，4，1
+ * why: 微任务优先
+ */
+// 宏任务
+setTimeout(function () {
+  console.log(1)
+}, 0);
+
+new Promise(function executor(resolve) {
+  // 直接执行
+  console.log(2);
+  // 直接执行
+  for (var i = 0; i < 10000; i++) {
+    i == 9999 && resolve(); // 微任务
+  }
+  // 直接执行
+  console.log(3);
+}).then(function () {
+  // 微任务
+  console.log(4);
+});
+// 宏任务
+console.log(5);
+
+/**
+ * Example 6
+ */
+console.log('golb1');
+
+setTimeout(function () {
+  console.log('timeout1');
+  process.nextTick(function () {
+    console.log('timeout1_nextTick');
+  })
+  new Promise(function (resolve) {
+    console.log('timeout1_promise');
+    resolve();
+  }).then(function () {
+    console.log('timeout1_then')
+  })
+})
+
+setImmediate(function () {
+  console.log('immediate1');
+  process.nextTick(function () {
+    console.log('immediate1_nextTick');
+  })
+  new Promise(function (resolve) {
+    console.log('immediate1_promise');
+    resolve();
+  }).then(function () {
+    console.log('immediate1_then')
+  })
+})
+
+process.nextTick(function () {
+  console.log('glob1_nextTick');
+})
+
+new Promise(function (resolve) {
+  console.log('glob1_promise');
+  resolve();
+}).then(function () {
+  console.log('glob1_then')
+})
+
+// 宏任务3
+setTimeout(function () {
+  console.log('timeout2');
+  process.nextTick(function () {
+    console.log('timeout2_nextTick');
+  })
+  new Promise(function (resolve) {
+    console.log('timeout2_promise');
+    resolve();
+  }).then(function () {
+    console.log('timeout2_then')
+  })
+})
+
+process.nextTick(function () {
+  console.log('glob2_nextTick');
+})
+
+new Promise(function (resolve) {
+  console.log('glob2_promise');
+  resolve();
+}).then(function () {
+  console.log('glob2_then')
+})
+
+// 宏任务4
+setImmediate(function () {
+  console.log('immediate2');
+  process.nextTick(function () {
+    console.log('immediate2_nextTick');
+  })
+  new Promise(function (resolve) {
+    console.log('immediate2_promise');
+    resolve();
+  }).then(function () {
+    console.log('immediate2_then')
+  })
+})
+
+/**
+ * Example 7
+ *
+node event-loop.js
+
+beginning of the program
+
+I am in the promise function!
+
+I am in the process next tick now
+
+I am in the first resolved promise
+
+I am in the second resolved promise
+
+I am in the callback from setTimeout with 0ms delay
+
+I am from setImmediate callback
+
+==================
+
+I am from setImmediate callback
+
+I am in the callback from setTimeout with 0ms delay
+
+*/
+
+/*
+beginning of the program
+
+*/
+const fs = require('fs');
+
+console.log('beginning of the program');
+
+const promise = new Promise(resolve => {
+  // function, passed to the Promise constructor
+  // is executed synchronously!
+  console.log('I am in the promise function!');
+  resolve('resolved message');
+});
+
+promise.then(() => {
+  console.log('I am in the first resolved promise');
+}).then(() => {
+  console.log('I am in the second resolved promise');
+});
+
+process.nextTick(() => {
+  console.log('I am in the process next tick now');
+});
+
+fs.readFile('index.html', () => {
+  console.log('==================');
+  setTimeout(() => {
+    console.log('I am in the callback from setTimeout with 0ms delay');
+  }, 0);
+
+  setImmediate(() => {
+    console.log('I am from setImmediate callback');
+  });
+});
+
+setTimeout(() => {
+  console.log('I am in the callback from setTimeout with 0ms delay');
+}, 0);
+
+setImmediate(() => {
+  console.log('I am from setImmediate callback');
+});
--- execution/EventLoop.md
+++ execution/EventLoop.md
@@ -10,16 +10,46 @@
 - 浏览器标准环境中（比如说谷歌webkit内核），是一个宏任务紧接着所有微任务执行。在node环境中，则又不一样了，是一个类型宏任务队列执行完，再去执行微任务。
 
 ## 宏观任务 Macro Task
-- 由宿主发起的任务称为宏观任务
-- script(整体代码), setTimeout, setInterval, setImmediate, I/O, UI rendering。
+由宿主发起的任务称为宏观任务
+- script
+- setTimeout
+- setInterval
+- setImmediate
+- I/O
+- UI
+- rendering
 
 ## 微观任务 Micro Task
-- 把JavaScript引擎发起的任务称为微观任务
-- process.nextTick, Promise, Object.observe(已废弃), MutationObserver(html5新特性)
+把JavaScript引擎发起的任务称为微观任务
+- process.nextTick
+- Promise
+- Object.observe(已废弃)
+- MutationObserver(html5新特性)
 
 ## 宏观任务与微观任务
 宏观任务的队列就相当于事件循环。
 每个宏观任务中包含了一个微观任务队列。
+
+## Node.js中的事件循环
+JavaScript在构建时考虑了异步行为，因此我们通常不会马上执行所有操作。以下列举的方法，事件不会直接按顺序执行:
+
+microtasks
+
+例如，立即处理Promises，如Promise.resolve。它意味着这段代码会在同一个的事件循环中被执行，但得等到所有同步代码执行完后。
+
+process.nextTick
+
+这是Node.js特有的方法，它不存在于任何浏览器（以及进程对象）中。它的行为类似于微任务(microtask)，但具有优先级。这意味着它将在所有同步代码之后立即执行，即使之前引入了其他微任务 - 这是很危险的，可能导致无限循环。从命名上讲是不对的，因为它是在同一个事件循环中执行的，而不是在它的next tick中执行。但是由于兼容性原因，它可能保持不变。
+
+setImmediate
+
+虽然它确实存在于某些浏览器中，但并未在所有浏览器中达到一致的行为，因此在浏览器中使用时，您需要非常小心。它类似于 setTimeout（0）代码，但有时会优先于它。这里的命名也不是最好的 - 我们在谈论下一个事件循环迭代，它并不是真正的immidiate。
+
+setTimeout/setInterval
+
+定时器在Node和浏览器中的表现形式是相同的。关于定时器的一个重要的事情是，我们提供的延迟不代表在这个时间之后回调就会被执行。它的真正含义是，一旦主线程完成所有操作（包括微任务）并且没有其它具有更高优先级的定时器，Node.js将在此时间之后执行回调。
+
+参考 Example 7
 
 ## 参考资料
 - [前端基础进阶（十二）：深入核心，详解事件循环机制](https://www.jianshu.com/p/12b9f73c5a4f)
