6abbbbdddbf9b6b04ef58fd6fa9879af8af23c4e
Wang Qiang <wangqiang0121@qq.com>
2020-08-27 07:30:21 +0800

    feat: 实现new运算符

--- Basic/new.js
+++ Basic/new.js
@@ -1,17 +1,29 @@
+/**
+ * JavaScript 原生 new 运算符的功能
+ */
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+}
+// new 支持构造函数及初始化参数
+const car1 = new Car('Eagle', 'Talon TSi', 1993);
+
+console.log(car1.make);
+
+/* 实现一个 类似new操作的函数，支持传入构造函数及初始化参数 */
 function create() {
 	// 创建一个空的对象
 	let obj = new Object()
-	// 获得构造函数
+	// 利用数组的shift获得第一个参数即构造函数，因为shift会对arguments直接操作，所以剩下的参数就是初始化参数。
   let Con = [].shift.call(arguments)
-  console.log(Con)
-	// 链接到原型
+	// 将新创建对象的__proto__属性指向构造函数的原型，确保新创建的对象可以访问其构造函数原型上的属性。
 	obj.__proto__ = Con.prototype
-	// 绑定 this，执行构造函数
+	// 构造函数的this绑定到新创建的对象上，并执行生成一个新的对象
 	let result = Con.apply(obj, arguments)
-	// 确保 new 出来的是个对象
+	// 确保最终返回的是一个对象
 	return typeof result === 'object' ? result : obj
 }
 
-const a = { aaa: 1 };
-const b = create(a);
-console.log(b);
+const car2 = create(Car, 'Eagle', 'Talon TSi', 1993);
+console.log(car2.model);
